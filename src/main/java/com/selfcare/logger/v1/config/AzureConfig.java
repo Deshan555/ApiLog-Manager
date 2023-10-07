4c45055423cca386b0adc6c35a1ec9143e57a1b7
Jayashanka Deshan <maxwon555@gmail.com>
2023-10-07 21:52:50 +0530

    configure azure blob storage properties

---
+++ src/main/java/com/selfcare/logger/v1/config/AzureConfig.java
@@ -0,0 +1,35 @@
+package com.selfcare.logger.v1.config;
+
+import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.BlobServiceClient;
+import com.azure.storage.blob.BlobServiceClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AzureConfig {
+    @Value("${azure.storage.connection.string}")
+    private String connectionString;
+
+    @Value("${azure.storage.container.name}")
+    private String containerName;
+    @Bean
+    public BlobServiceClient clobServiceClient() {
+
+        BlobServiceClient blobServiceClient =
+                new BlobServiceClientBuilder()
+                        .connectionString(connectionString)
+                        .buildClient();
+
+        return blobServiceClient;
+    }
+    @Bean
+    public BlobContainerClient blobContainerClient() {
+
+        BlobContainerClient blobContainerClient =
+                clobServiceClient()
+                        .getBlobContainerClient(containerName);
+        return blobContainerClient;
+    }
+}
