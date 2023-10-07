1cdc3755d2bfabff98cd3ccc73acd1c4c6f5db1d
Jayashanka Deshan <maxwon555@gmail.com>
2023-10-07 21:24:45 +0530

    init git repository

---
+++ src/main/java/com/selfcare/logger/v1/Logger.java
@@ -0,0 +1,13 @@
+package com.selfcare.logger.v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Logger {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Logger.class, args);
+	}
+
+}
---
+++ src/test/java/com/selfcare/logger/v1/LoggerTests.java
@@ -0,0 +1,13 @@
+package com.selfcare.logger.v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggerTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
